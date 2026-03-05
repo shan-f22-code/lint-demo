e2053102f8ecf966d3ca0e7beabfd82d24ad2d6d
shan-f22-code <shanmugapriyan@f22labs.com>
2026-03-05 11:18:06 +0000

    docs: add comments to math functions

--- math.js
+++ math.js
@@ -1,9 +1,11 @@
-function add(a,b){
-return a+b
+// Returns the sum of two numbers.
+function add(a, b) {
+  return a + b
 }
 
-function subtract(a,b){
-return a-b
+// Returns the difference of two numbers.
+function subtract(a, b) {
+  return a - b
 }
 
-module.exports={add,subtract}
+module.exports = { add, subtract }
